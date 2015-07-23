fe46bdab66a10d60c6b8560cd8676a2c1374094e
Joe Ferraro <ferraro.joseph@gmail.com>
2015-07-23 12:13:47 -0400

    adds menu, stops server if running

--- app/main.js
+++ app/main.js
@@ -1,7 +1,9 @@
 var app = require('app');  // Module to control application life.
 var autoUpdater = require('auto-updater');
 var path = require('path');
+var Menu = require('menu');
 var BrowserWindow = require('browser-window');  // Module to create native browser window.
+var mavensmate = require('mavensmate');
 
 // autoUpdater.setFeedUrl('http://mycompany.com/myapp/latest?version=' + app.getVersion());
 
@@ -27,6 +29,8 @@ var openUrlInNewTab = function(url) {
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is GCed.
 var mainWindow = null;
+var mavensMateServer = null;
+var applicationMenu = null;
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
@@ -49,21 +53,161 @@ app.on('ready', function() {
     'min-height': 800
   });
 
+  if (!Menu.getApplicationMenu()) {
+    var template;
+    if (process.platform == 'darwin') {
+      template = [
+        {
+          label: 'MavensMate',
+          submenu: [
+            {
+              label: 'Services',
+              submenu: []
+            },
+            {
+              type: 'separator'
+            },
+            {
+              label: 'Hide MavensMate',
+              accelerator: 'Command+H',
+              selector: 'hide:'
+            },
+            {
+              label: 'Hide Others',
+              accelerator: 'Command+Shift+H',
+              selector: 'hideOtherApplications:'
+            },
+            {
+              label: 'Show All',
+              selector: 'unhideAllApplications:'
+            },
+            {
+              type: 'separator'
+            },
+            {
+              label: 'Quit',
+              accelerator: 'Command+Q',
+              click: function() { app.quit(); }
+            },
+          ]
+        },
+        {
+          label: 'Edit',
+          submenu: [
+            {
+              label: 'Undo',
+              accelerator: 'Command+Z',
+              selector: 'undo:'
+            },
+            {
+              label: 'Redo',
+              accelerator: 'Shift+Command+Z',
+              selector: 'redo:'
+            },
+            {
+              type: 'separator'
+            },
+            {
+              label: 'Cut',
+              accelerator: 'Command+X',
+              selector: 'cut:'
+            },
+            {
+              label: 'Copy',
+              accelerator: 'Command+C',
+              selector: 'copy:'
+            },
+            {
+              label: 'Paste',
+              accelerator: 'Command+V',
+              selector: 'paste:'
+            },
+            {
+              label: 'Select All',
+              accelerator: 'Command+A',
+              selector: 'selectAll:'
+            },
+          ]
+        },
+        {
+          label: 'Window',
+          submenu: [
+            {
+              label: 'Minimize',
+              accelerator: 'Command+M',
+              selector: 'performMiniaturize:'
+            },
+            {
+              label: 'Close',
+              accelerator: 'Command+W',
+              selector: 'performClose:'
+            },
+            {
+              type: 'separator'
+            },
+            {
+              label: 'Bring All to Front',
+              selector: 'arrangeInFront:'
+            },
+          ]
+        },
+        {
+          label: 'Help',
+          submenu: [
+            {
+              label: 'Learn More',
+              click: function() { require('shell').openExternal('http://mavensmate.com') }
+            }
+          ]
+        }
+      ];
+    } else {
+      template = [
+        {
+          label: '&File',
+          submenu: [
+            {
+              label: '&Close',
+              accelerator: 'Ctrl+W',
+              click: function() {
+                var focusedWindow = BrowserWindow.getFocusedWindow();
+                if (focusedWindow)
+                  focusedWindow.close();
+              }
+            },
+          ]
+        },
+        {
+          label: 'Help',
+          submenu: [
+            {
+              label: 'Learn More',
+              click: function() { require('shell').openExternal('http://mavensmate.com') }
+            }
+          ]
+        }
+      ];
+    }
+
+    var menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
+  }
+
   // and load the index.html of the app.
   mainWindow.loadUrl('file://' + __dirname + '/index.html');
 
   mainWindow.webContents.on('did-finish-load', function() {
-    // mainWindow.webContents.send('ping', 'whoooooooh!');
-    // mainWindow.webContents.send('openTab', 'http://localhost:56248/app/home/index');
-
-    var mavensmate = require('mavensmate');
+    if (mavensMateServer && mavensMateServer.stop) { // happens when app is restarted
+      mavensMateServer.stop();
+    }
     mavensmate
       .startServer({
         editor: 'sublime',
         port: 56248,
         windowOpener: openUrlInNewTab
       })
-      .then(function() {
+      .then(function(server) {
+        mavensMateServer = server;
         mainWindow.webContents.send('openTab', 'http://localhost:56248/app/home/index');
       })
       .catch(function(err) {
