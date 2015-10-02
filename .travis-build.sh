768446629348dd846558a9987b4fba3d76f7df4b
Joe Ferraro <ferraro.joseph@gmail.com>
2015-10-02 13:22:01 -0400

    add certtool to help debug

--- .travis-build.sh
+++ .travis-build.sh
@@ -55,6 +55,7 @@ if [ "$TRAVIS_OS_NAME" = "linux" -o -z "$TRAVIS_OS_NAME" ]; then
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
     npm run build:osx
     # TODO: sign ../dist/osx/MavensMate-darwin-x64/MavensMate.app
+    certtool y | grep Developer\ ID
     codesign --deep --force --verbose --keychain ~/Library/Keychains/mavensmate-app-build.keychain --sign "Developer ID Application: Joseph Ferraro ($APPLE_TEAM_ID)" ../dist/osx/MavensMate-darwin-x64/MavensMate.app
     codesign --verify -vvvv ../dist/osx/MavensMate-darwin-x64/MavensMate.app
     npm run pack-only:osx
