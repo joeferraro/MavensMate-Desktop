075eebe556b46219380b9c5d157c0999d88fa176
Joe Ferraro <ferraro.joseph@gmail.com>
2016-06-08 15:36:52 -0400

    update build script to depend on npm shipped with node 4

--- .travis-build.sh
+++ .travis-build.sh
@@ -24,17 +24,13 @@ elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
     ./scripts/add-key.sh
 fi
 
-cd ..
-curl -L "https://github.com/npm/npm/archive/v3.2.0.tar.gz" >> npm3.tar.gz
-tar -xzvf npm3.tar.gz
-ls
-cd MavensMate-app/app
-node ../../npm-3.2.0/bin/npm-cli.js install -g node-gyp-install
+cd app
+npm install -g node-gyp-install
 node-gyp-install
-node ../../npm-3.2.0/bin/npm-cli.js install
+npm install
 ls
-../../npm-3.2.0/bin/npm-cli.js install electron-builder -g
-../../npm-3.2.0/bin/npm-cli.js install electron-packager -g
+npm install electron-builder -g
+npm install electron-packager -g
 ./node_modules/.bin/electron-rebuild
 
 #if OS is linux or is not set
@@ -77,13 +73,13 @@ elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
     sudo codesign --deep --force --verbose --keychain ~/Library/Keychains/mavensmate.keychain --sign "$APP_KEY" "../dist/osx/MavensMate-darwin-x64/MavensMate.app/Contents/Frameworks/Electron Framework.framework"
     sudo codesign --deep --force --verbose --keychain ~/Library/Keychains/mavensmate.keychain --sign "$APP_KEY" "../dist/osx/MavensMate-darwin-x64/MavensMate.app"
     sudo codesign --verify -vvvv ../dist/osx/MavensMate-darwin-x64/MavensMate.app
-    
+
     npm run pack-only:osx
-        
+
     sudo codesign --deep --force --verbose --keychain ~/Library/Keychains/mavensmate.keychain --sign "$APP_KEY" "../dist/osx/MavensMate.dmg"
 
     sudo codesign --verify -vvvv ../dist/osx/MavensMate.dmg
-    
+
     cd ../dist/osx
     ls
     zip MavensMate-app-$TRAVIS_TAG-osx-x64.zip MavensMate.dmg
